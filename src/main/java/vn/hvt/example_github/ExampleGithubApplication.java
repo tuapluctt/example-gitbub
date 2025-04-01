7c5c7b24a7595ca1903846503e234b3f26a9e68c
tuapluctt <hoangvantunkv@gmail.com>
2025-04-01 12:18:07 +0700

    Initial commit

---
+++ src/main/java/vn/hvt/example_github/ExampleGithubApplication.java
@@ -0,0 +1,13 @@
+package vn.hvt.example_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/hvt/example_github/ExampleGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hvt.example_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
